f4741bc3ed64df572295882e8509b7095317c5e3
shgang <shgang97@163.com>
2020-05-29 15:09:10 +0800

    bytedance

---
+++ bytedance.py
@@ -0,0 +1,8 @@
+# -How to prove that you are a programmer？
+# -Output HelloWorld on the screen.
+# -How to prove that you are an excellent programmer？
+# -Output bytedance 1024 times on the screen with loop statement
+
+
+for i in range(1024):
+    print("bytedance")
